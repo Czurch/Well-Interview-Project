174c24d53bf5eaf491a9a03b76eae71a527e3c9d
Luke Ferreira <lukesferreira1217@gmail.com>
2023-11-05 21:36:31 -0500

    SQLite DB with cache

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
 import express from "express";
+import Database from "better-sqlite3";
+import cache from "memory-cache";
 
 const app = express();
 app.use(express.text());
 const port = 9000;
 
-const freqMap = new Map();
+const db = new Database("freqMap.db");
+
+db.prepare(
+  "CREATE TABLE IF NOT EXISTS freqMap (key TEXT PRIMARY KEY, value INTEGER)"
+).run();
 
 app.post("/input", (req, res) => {
   if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
     return res.status(400).send("Key can not be null");
   }
-  if (freqMap.has(req.body)) {
-    const value = freqMap.get(req.body);
-    freqMap.set(req.body, value + 1);
-  } else {
-    freqMap.set(req.body, 1);
-  }
-  console.log(`${req.body}: ${freqMap.get(req.body)}`);
+
+  const keyString = req.body.toString();
+  const inputStatement = db.prepare(
+    "INSERT OR REPLACE INTO freqMap (key, value) VALUES (?, COALESCE((SELECT value FROM freqMap WHERE key = ?), 0) + 1)"
+  );
+
+  inputStatement.run(`${keyString}`, `${keyString}`);
+  console.log(`${keyString}`);
+
+  //Invalidate cache for this key
+  cache.del(keyString);
+
   return res.send("SUCCESS");
 });
 
 app.get("/query", (req, res) => {
   if (!req.query.key) return res.status(400).send("Key can not be null");
-  if (freqMap.has(req.query.key))
-    return res.send(`${freqMap.get(req.query.key)}`);
-  else return res.send("0");
+
+  const cachedValue = cache.get(req.query.key);
+  if (cachedValue !== null) {
+    //If in cache don't hit the DB
+    return res.send(cachedValue.toString());
+  }
+
+  const queryStatment = db.prepare("SELECT value FROM freqMap WHERE key = ?");
+  const keyString = req.query.key.toString();
+  const row = queryStatment.get(`${keyString}`);
+
+  if (row) {
+    return res.send(row.value.toString());
+  } else {
+    return res.send("0");
+  }
 });
 
 app.listen(port, () => {
